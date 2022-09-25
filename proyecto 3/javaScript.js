2e734e8deff92a73a681144fe0cbef1f925794c0
FacundoOscarBarrera <110206291+FacundoOscarBarrera@users.noreply.github.com>
2022-09-25 19:38:51 -0300

    proyecto 3

---
+++ proyecto 3/javaScript.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $(window).on('load', function () {
+                 $('#loading').fadeOut(1500);
+                    $('html').css('overflow-y','visible');
+});
+    
+}(jQuery));
+
